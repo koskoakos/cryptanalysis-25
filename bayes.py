b75bded49f07ce277bbb0f131110fc32b5d2d50a
Oleg Si <ksoksodzhi@gmail.com>
2025-09-29 17:37:02 +0300

    Added deterministic dB

--- bayes.py
+++ bayes.py
@@ -17,3 +17,22 @@ print(f"{P_K.sum()=}")
 E = pd.read_csv(TABLE_CSV, header=None).values
 print(E[:2])
 
+P_C = np.zeros(20, dtype=float)
+P_joint = np.zeros((20, 20), dtype=float)
+
+for key in range(20):
+    for m in range(20):
+        c = E[key, m]
+        mk = P_M[m] * P_K[key]
+        P_C[c] += mk
+        P_joint[m, c] += mk
+
+P_post = P_joint/P_C
+
+d_B = np.argmax(P_post, axis=0)
+print(f"{d_B=}")
+
+loss_D = float(np.sum(P_C * (1.0 - P_post.max(axis=0))))
+print(f"{loss_D=}")
+
+
