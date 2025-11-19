5e5dbe9a331fe31e86c1744eee2cc98a01084b97
Oleg Si <ksoksodzhi@gmail.com>
2025-11-19 23:15:40 +0200

    EZ SE attack done

--- lab3/se.py
+++ lab3/se.py
@@ -22,12 +22,34 @@ NN = reduce(lambda x, y: x*y, N)
 N_star = [NN // n_i for n_i in N]
 
 # %%
-for n1 in N:
-    for n2 in N:
-        print(gcd(n1, n2))
+C_inv = [pow(n_s, -1, n_i) for n_s, n_i in zip(N_star, N)]
 
 # %%
-gcd(N[0], N[1])
+C_crt = sum(c_i * n_s * c_i_inv for c_i, n_s, c_i_inv in zip(C, N_star, C_inv)) % NN
+
+# %%
+a = 0
+b = C_crt
+while True:
+    mid = (a + b) // 2
+    if pow(mid, 3, NN) < C_crt:
+        a = mid
+    elif pow(mid, 3, NN) > C_crt:
+        b = mid
+    elif pow(mid, 3, NN) == C_crt:
+        print(f"Found exact root: {mid}")
+        break
+    
+
+# %%
+bytelen = (mid.bit_length() + 7) // 8
+plaintext = mid.to_bytes(bytelen, 'big')
+
+# %%
+plaintext.split(b'\x00')[-1]
+
+# %%
+plaintext
 
 # %%
 
